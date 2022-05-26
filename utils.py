b4ec27947d750827240e1dc526b771735cfb68fa
friedrichor <kongfanheng426@163.com>
2022-05-26 17:14:59 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,47 +18,49 @@ import params
 
 # 划分训练集和验证集
 def split_train_val(root: str, val_rate: float = 0.2):
-    # 获取类别
-    classes = [cls for cls in os.listdir(root)]
+    random.seed(0)  # 保证随机结果可复现
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    # 遍历文件夹，一个文件夹对应一个类别
+    classes = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
     # 排序，保证顺序一致
     classes.sort()
     # 生成类别名称以及对应的数字索引
-    # 因为后面要通过cls查找其索引，所以class_indices的key,value按此定义
-    class_indices = dict((cls, index) for index, cls in enumerate(classes))
-    json_str = json.dumps(dict((index, cls) for cls, index in class_indices.items()), indent=4)
-    with open(params.path_json, 'w') as json_file:
+    class_indices = dict((k, v) for v, k in enumerate(classes))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open(str(params.ROOT / 'class_indices.json'), 'w') as json_file:
         json_file.write(json_str)
 
-    train_images_path = []
-    train_images_label = []
-    val_images_path = []
-    val_images_label = []
-    every_class_num = []
-
-    for cls in classes:
-        cls_path = os.path.join(root, cls)
-        # 获取当前类别的所有图片路径
-        images_path = [os.path.join(root, cls, img_name)
-                       for img_name in os.listdir(cls_path)]
+    train_images_path = []  # 存储训练集的所有图片路径
+    train_images_label = []  # 存储训练集图片对应索引信息
+    val_images_path = []  # 存储验证集的所有图片路径
+    val_images_label = []  # 存储验证集图片对应索引信息
+    every_class_num = []  # 存储每个类别的样本总数
+    # 遍历每个文件夹下的文件
+    for cla in classes:
+        cla_path = os.path.join(root, cla)
+        # 遍历获取supported支持的所有文件路径
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)]
         # 获取该类别对应的索引
-        images_class = class_indices[cls]
+        image_class = class_indices[cla]
         # 记录该类别的样本数量
-        every_class_num.append(len(images_path))
-        # 按比例随机划分训练集和验证集, random_state=0固定, 保证每次划分的数据集都一样
-        train_path, val_path = train_test_split(images_path, test_size=0.2, random_state=0)
-        train_label = [images_class] * len(train_path)
-        val_label = [images_class] * len(val_path)
+        every_class_num.append(len(images))
+        # 按比例随机采样验证样本
+        val_path = random.sample(images, k=int(len(images) * val_rate))
 
-        train_images_path = np.hstack((train_images_path, train_path))
-        train_images_label = np.hstack((train_images_label, train_label))
-        val_images_path = np.hstack((val_images_path, val_path))
-        val_images_label = np.hstack((val_images_label, val_label))
+        if os.path.exists(params.ROOT / 'val' / cla):
+            shutil.rmtree(params.ROOT / 'val' / cla)
+        os.makedirs(params.ROOT / 'val' / cla)
 
-        if os.path.exists(params.ROOT / 'val' / cls):
-            shutil.rmtree(params.ROOT / 'val' / cls)
-        os.makedirs(params.ROOT / 'val' / cls)
-        for val_image_path in val_images_path:
-            shutil.copy(val_image_path, params.ROOT / 'val' / cls)
+        for img_path in images:
+            if img_path in val_path:  # 如果该路径在采样的验证集样本中则存入验证集
+                val_images_path.append(img_path)
+                val_images_label.append(image_class)
+                shutil.copy(img_path, params.ROOT / 'train' / cla)
+            else:  # 否则存入训练集
+                train_images_path.append(img_path)
+                train_images_label.append(image_class)
+                shutil.copy(img_path, params.ROOT / 'val' / cla)
 
     print("{} images were found in the dataset.".format(sum(every_class_num)))
     print("{} images for training.".format(len(train_images_path)))
